b4c59e75b81b97e05e3381c8ce7c63555604e635
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-18 12:17:14 -0300

    feat: add calculator class to calculate basic operations

---
+++ checkpoint/Checkpoint-1/Checkpoint-1/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkpoint_1
+{
+
+    internal class Calculator
+    {
+        public double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+        
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
