e8215c620e062fb1a7915c24eb846c329a7311ef
kayquesan2 <rm552605@fiap.com.br>
2024-09-17 23:24:19 -0300

    feat: add AgeCalculator to calculate user age.

---
+++ checkpoint/Checkpoint-1/AgeCalculator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeCalculator
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Digite seu nome completo: ");
+                String name = Console.ReadLine();
+
+                Console.Write("Digite sua data de nascimento (formato: dd/MM/yyyy): ");
+                String birthDataInput = Console.ReadLine();
+
+                DateTime birthData;
+                bool validDate = DateTime.TryParseExact(birthDataInput, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out birthData);
+
+                if (!validDate)
+                {
+                    Console.WriteLine("Data de nascimento inválida. Por favor, tente novamente.");
+                }
+                else
+                {
+                    int age = CalculateAge(birthData);
+                    Console.WriteLine($"{name}, você tem {age} anos");
+                    Console.WriteLine("Fim do Programa");
+                    Console.ReadKey();
+                    break;
+
+                }
+                
+                Console.ReadKey();
+            }
+
+        }
+
+       static int CalculateAge(DateTime birthData)
+        {
+            DateTime currentDate = DateTime.Now;
+            int age = currentDate.Year - birthData.Year;
+
+            if (currentDate < birthData.AddYears(age))
+            {
+                age--;
+            }
+            return age;
+        }
+
+
+    }
+}
