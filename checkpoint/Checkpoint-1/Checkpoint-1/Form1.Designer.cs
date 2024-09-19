f2ef3fc47defc78e084857178d94e22304bfbe7f
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-19 16:54:53 -0300

    feat: call function when operations buttons is clicked to handle event

--- checkpoint/Checkpoint-1/Checkpoint-1/Form1.Designer.cs
+++ checkpoint/Checkpoint-1/Checkpoint-1/Form1.Designer.cs
@@ -191,6 +191,7 @@
             this.button12.TabIndex = 1;
             this.button12.Text = "+";
             this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button1_Click);
             // 
             // button13
             // 
@@ -202,6 +203,7 @@
             this.button13.TabIndex = 1;
             this.button13.Text = "-";
             this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button1_Click);
             // 
             // button14
             // 
@@ -213,6 +215,7 @@
             this.button14.TabIndex = 1;
             this.button14.Text = "X";
             this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button1_Click);
             // 
             // button15
             // 
@@ -224,6 +227,7 @@
             this.button15.TabIndex = 1;
             this.button15.Text = "÷";
             this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button1_Click);
             // 
             // button16
             // 
@@ -247,6 +251,7 @@
             this.button17.TabIndex = 1;
             this.button17.Text = "%";
             this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.button1_Click);
             // 
             // button18
             // 
@@ -259,6 +264,7 @@
             this.button18.TabIndex = 1;
             this.button18.Text = "C";
             this.button18.UseVisualStyleBackColor = false;
+            this.button18.Click += new System.EventHandler(this.button1_Click);
             // 
             // button19
             // 
