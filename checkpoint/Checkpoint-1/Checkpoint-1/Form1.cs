858cbe70f13cd20669ac751e1448a725ed3520b6
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-19 16:57:33 -0300

    feat: handle button event click to determine the action to take

--- checkpoint/Checkpoint-1/Checkpoint-1/Form1.cs
+++ checkpoint/Checkpoint-1/Checkpoint-1/Form1.cs
@@ -18,8 +18,77 @@ namespace Checkpoint_1
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
+        { 
+            Calculator calculator = new Calculator();
 
+            Button button = (Button)sender;
+            
+            String[] listMathOperations = {"+", "-", "X", "÷"};
+            String firstOperator = "";
+            String secondOperator = "";
+            String operation = "";
+            double result = 0;
+
+            if (listMathOperations.Contains(button.Text))
+            {
+                int operationIndex = Array.IndexOf(listMathOperations, button.Text);
+                operation = listMathOperations[operationIndex];
+            }
+            else if(button.Text.Equals(","))
+            {
+                if(operation == "")
+                {
+                    if (!firstOperator.Contains(",") && firstOperator != "")
+                    {
+                        firstOperator.Concat(",");
+                    }
+                }
+                else
+                {
+                    if (!secondOperator.Contains(",") && secondOperator != "")
+                    {
+                        secondOperator.Concat(",");
+                    }
+                }
+            }
+            else if (button.Text.Equals("C"))
+            {
+                firstOperator = "";
+                secondOperator = "";
+                operation = "";
+            }
+            else if (button.Text.Equals("="))
+            {
+               if(firstOperator != "" && secondOperator != "")
+                {
+                    double firstOperatorNumber = Double.Parse(firstOperator);
+                    double secondOperatorNumber = Double.Parse(secondOperator);
+               
+
+                    if (operation == "+")
+                    {
+                        result = calculator.Sum(firstOperatorNumber, secondOperatorNumber);
+                    }
+                    else if(operation == "-")
+                    {
+                        result = calculator.Subtract(firstOperatorNumber, secondOperatorNumber);
+                    }
+                }
+            }
+            else
+            {
+                if(operation == "")
+                {
+                    firstOperator.Concat(button.Text);
+                }
+                else
+                {
+                    secondOperator.Concat(button.Text);
+                }
+            }
+
+            this.textBox1.Text = result != 0 ? result.ToString() : firstOperator + " " + operation + " " + secondOperator;
+            Console.WriteLine(this.textBox1.Text);
         }
 
         private void Form1_Load(object sender, EventArgs e)
