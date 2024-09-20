81aa8cc32d90bf3b3a9680bbc40025a19a580640
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-20 17:11:31 -0300

    feat: encapsulate display result logic into class

---
+++ checkpoint/Checkpoint-1/Checkpoint-1/command/DisplayResultCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Checkpoint_1.command
+{
+    internal class DisplayResultCommand
+    {
+
+        private TextBox textBox;
+
+        private Calculator calculator;
+        public DisplayResultCommand(Calculator calculator, TextBox textBox) 
+        {
+            this.calculator = calculator;
+            this.textBox = textBox;
+        }
+
+        public void Execute()
+        {
+            if (calculator.result != 0)
+            {
+                textBox.Text = calculator.result.ToString();
+                calculator.firstOperator = calculator.result.ToString();
+                calculator.secondOperator = "";
+                calculator.operation = "";
+                calculator.result = 0;
+            }
+            else
+            {
+                textBox.Text = calculator.firstOperator + " " + calculator.operation + " " + calculator.secondOperator;
+            }
+        }
+    }
+}
