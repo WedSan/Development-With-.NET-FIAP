a8d572f439f9cd41b59564faccf59433d9e06347
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-20 17:11:15 -0300

    feat: encapsulate clear calculator fields logic into class

---
+++ checkpoint/Checkpoint-1/Checkpoint-1/command/ClearFieldCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkpoint_1.command
+{
+    internal class ClearFieldCommand
+    {
+        private Calculator calculator;
+
+        public ClearFieldCommand(Calculator calculator)
+        {
+            this.calculator = calculator;
+        }
+
+        public void Execute()
+        {
+            calculator.firstOperator = "";
+            calculator.secondOperator = "";
+            calculator.operation = "";
+            calculator.result = 0;
+        }
+    }
+}
