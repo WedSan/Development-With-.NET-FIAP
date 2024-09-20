da824aa3bc519e5285e8a2738b54f5743c79c4ab
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-20 17:10:32 -0300

    feat: encapsulate add operator logic into class

---
+++ checkpoint/Checkpoint-1/Checkpoint-1/command/AddOperatorCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Checkpoint_1.command
+{
+    internal class AddOperatorCommand
+    {
+        private Calculator calculator;
+
+        private Button button;
+
+        public AddOperatorCommand(Calculator calculator, Button buttonEvent)
+        {
+            this.calculator = calculator;
+            this.button = buttonEvent;
+        }
+
+        public void Execute()
+        {
+            if (calculator.operation == "")
+            {
+                calculator.firstOperator += button.Text;
+            }
+            else
+            {
+                calculator.secondOperator += button.Text;
+            }
+        }
+    }
+
+   
+}
