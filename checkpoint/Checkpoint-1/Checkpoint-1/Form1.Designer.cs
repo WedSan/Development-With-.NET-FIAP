4598fb855998dd3a63e44b573ca1fca13f4162d1
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-20 03:15:20 -0300

    fix: add function to handle event click to calculate the result of the math expression

--- checkpoint/Checkpoint-1/Checkpoint-1/Form1.Designer.cs
+++ checkpoint/Checkpoint-1/Checkpoint-1/Form1.Designer.cs
@@ -72,6 +72,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "=";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
