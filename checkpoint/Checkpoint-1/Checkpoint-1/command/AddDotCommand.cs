bb52965cd0338e1a94962af8ef75dfe54641e7b1
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-20 17:10:22 -0300

    feat: encapsulate add dot logic into class

---
+++ checkpoint/Checkpoint-1/Checkpoint-1/command/AddDotCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkpoint_1.command
+{
+    internal class AddDotCommand
+    {
+        private Calculator calculator;
+        
+        public AddDotCommand(Calculator calculator) 
+        {
+            this.calculator = calculator;
+        }
+
+        public void Execute()
+        {
+            if (calculator.operation == "")
+            {
+                if (!calculator.firstOperator.Contains(".") && calculator.firstOperator != "")
+                {
+                    calculator.firstOperator += ".";
+                }
+            }
+            else
+            {
+                if (!calculator.secondOperator.Contains(".") && calculator.secondOperator != "")
+                {
+                    calculator.secondOperator += ".";
+                }
+            }
+        }
+    }
+}
