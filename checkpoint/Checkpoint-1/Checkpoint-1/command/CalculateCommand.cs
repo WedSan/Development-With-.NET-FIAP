ee44ed048dfa429741630ed14c3952adc3c59d2e
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-20 17:11:01 -0300

    feat: encapsulate calculation logic into class

---
+++ checkpoint/Checkpoint-1/Checkpoint-1/command/CalculateCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkpoint_1.command
+{
+    internal class CalculateCommand
+    {
+
+        private Calculator calculator;
+        public CalculateCommand(Calculator calculator) 
+        {
+            this.calculator = calculator;
+        }
+
+        public void Execute()
+        {
+            if (calculator.firstOperator != "" && calculator.secondOperator != "")
+            {
+                double firstOperatorNumber = Double.Parse(calculator.firstOperator);
+                double secondOperatorNumber = Double.Parse(calculator.secondOperator);
+
+
+                if (calculator.operation == "+")
+                {
+                    calculator.result = calculator.Sum(firstOperatorNumber, secondOperatorNumber);
+                }
+                else if (calculator.operation == "-")
+                {
+                    calculator.result = calculator.Subtract(firstOperatorNumber, secondOperatorNumber);
+                }
+                else if (calculator.operation == "X")
+                {
+                    calculator.result = calculator.Multiply(firstOperatorNumber, secondOperatorNumber);
+                }
+                else if (calculator.operation == "÷")
+                {
+                    calculator.result = calculator.Divide(firstOperatorNumber, secondOperatorNumber);                    
+                }
+                else if (calculator.operation == "%")
+                {
+                    calculator.result = calculator.CalculatePercentage(firstOperatorNumber, secondOperatorNumber);
+                }
+            }
+        }
+    }
+}
