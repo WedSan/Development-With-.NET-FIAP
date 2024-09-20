dab68388296a3f89b90eddf52a77868f58a747f4
WedSan <andre.alves.silvazesez@gmail.com>
2024-09-20 17:11:44 -0300

    feat: encapsulate reverse operator signal logic into class

---
+++ checkpoint/Checkpoint-1/Checkpoint-1/command/InverseSignalCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkpoint_1.command
+{
+    internal class InverseSignalCommand
+    {
+        private Calculator calculator;
+
+        public InverseSignalCommand(Calculator calculator)
+        {
+            this.calculator = calculator;
+        }
+
+
+        public void Execute()
+        {
+            if (calculator.operation == "")
+            {
+                if (calculator.firstOperator.Contains("-"))
+                {
+                    calculator.firstOperator = calculator.firstOperator.Remove(0, 1);
+                }
+                else if (calculator.firstOperator != "")
+                {
+                    calculator.firstOperator = calculator.firstOperator.Insert(0, "-");
+                }
+            }
+            else
+            {
+                if (calculator.secondOperator.Contains("-"))
+                {
+                    calculator.secondOperator = calculator.secondOperator.Remove(0, 1);
+                }
+                else if (calculator.secondOperator != "")
+                {
+                    calculator.secondOperator = calculator.secondOperator.Insert(0, "-");
+                }
+            }
+        }
+    }
+}
