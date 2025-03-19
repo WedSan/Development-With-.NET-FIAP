e04497a299288bc13bd5bb0dd597a388591d1760
WedSan <andre.alves.silvazesez@gmail.com>
2025-03-19 11:39:43 -0300

    feat: init checkpoint 1 2 semester project

---
+++ checkpoint/Checkpoint-1-2-semester/checkpoint-1-2-semester/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using checkpoint_1_2_semester.Models;
+
+namespace checkpoint_1_2_semester.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ checkpoint/Checkpoint-1-2-semester/checkpoint-1-2-semester/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace checkpoint_1_2_semester.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
