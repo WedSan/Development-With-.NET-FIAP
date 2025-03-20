b60bf90e7e94e1170d6309e8527cd2af43695895
WedSan <andre.alves.silvazesez@gmail.com>
2025-03-20 14:38:33 -0300

    chore: add Entity Framework Core and Npgsql packages for PostgreSQL support

--- checkpoint/Checkpoint-1-2-semester/checkpoint-1-2-semester/Program.cs
+++ checkpoint/Checkpoint-1-2-semester/checkpoint-1-2-semester/Program.cs
@@ -1,9 +1,30 @@
+using checkpoint_1_2_semester.Repositories;
+using checkpoint_1_2_semester.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDBContext>(options =>
+              options.UseNpgsql(Environment.GetEnvironmentVariable("ConnectionString")));
+
+builder.Services.AddScoped<IToyRepository, ToyRepository>();
+builder.Services.AddScoped<IToyService, ToyService>();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDBContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -22,6 +43,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Toys}/{action=Index}/{id?}");
 
 app.Run();
